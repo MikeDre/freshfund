79462f1900e97853c69e82f0f53f0b477c99f6f7
MikeDre <info@mikedre.com>
2021-01-28 17:49:02 +0000

    [main] Added footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import SocialIcons from './SocialIcons/SocialIcons';
+import CourierMailchimpLogo from '../images/courier-mailchimp.svg';
 
 const footer = () => (
-  <footer className="u-background-color-black-off uk-padding-large">
+  <footer className="u-background-color-black uk-padding-large">
     <div className="uk-container u-color-white">
       <div className="uk-grid" data-uk-grid>
         <div className="uk-width-2-3@m">
-          [ Footer ]
+          <img src={CourierMailchimpLogo} alt="Courier and Mailchimp Logo" width="270" />
+          <div className="uk-margin-large-top">
+            <small className="footer__text u-color-white">
+              &copy; 2001 - 2021 ALL RIGHTS RESERVED
+            </small>
+          </div>
         </div>
-        <div className="uk-width-1-3@m">
+        <div className="uk-width-1-3@m uk-flex uk-flex-right">
           <SocialIcons
             darkMode={false}
             instagram="https://instagram.com/freshfund"
--- src/components/Video/VideoHero.js
+++ src/components/Video/VideoHero.js
@@ -10,7 +10,7 @@ class VideoHero extends React.Component {
       <div className="video-hero">
         <div className="wistia_responsive_padding">
           <div className="wistia_responsive_wrapper">
-            <iframe src={`https://fast.wistia.net/embed/iframe/${this.props.wistiaID}?videoFoam=true`} title="Fresh Fund Compilation - (with captions) Video" allow="autoplay; fullscreen" allowtransparency="true" frameBorder="0" scrolling="no" className="wistia_embed" name="wistia_embed" allowFullScreen msallowfullscreen width="100%" height="100%" />
+            <iframe src={`https://fast.wistia.net/embed/iframe/${this.props.wistiaID}?videoFoam=true`} title="Fresh Fund Compilation - (with captions) Video" allow="autoplay; fullscreen" allowtransparency="true" frameBorder="0" scrolling="no" className="wistia_embed" name="wistia_embed" allowFullScreen data-msallowfullscreen width="100%" height="100%" />
           </div>
         </div>
       </div>
